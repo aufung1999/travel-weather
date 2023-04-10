aede45623521ef1c41409c10b82f9592f4c94b7b
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-04-10 15:27:43 -0400

    update

--- redux/actions/actions.js
+++ redux/actions/actions.js
@@ -6,7 +6,7 @@ export const loadInitalDataAction = () => {
   return async (dispatch, getState) => {
     // Fetching results from an API : asynchronous action
     const response = await fetch(
-      "http://api.openweathermap.org/data/2.5/forecast?lat=42.985900&lon=-81.294700&appid=6e0f30fd9824b943d84e28b935ec6e4d&units=metric&cnt=40"
+      "https://api.openweathermap.org/data/2.5/forecast?lat=42.985900&lon=-81.294700&appid=6e0f30fd9824b943d84e28b935ec6e4d&units=metric&cnt=40"
     );
     const data = await response.json();
 
